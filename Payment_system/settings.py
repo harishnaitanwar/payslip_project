fc1a46bc74a1fdc73dc8cfac3fb2e54ca0ce3a40
JyothiVutukuri333 <139746072+JyothiVutukuri333@users.noreply.github.com>
2024-12-26 16:11:11 +0530

    name (#11)
    New file is added #jyothi.txt file is added
    modified one file which is settings.py
    added// jyo //

--- Payment_system/settings.py
+++ Payment_system/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = Path.joinpath(BASE_DIR / 'Employee_data/media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+// jyo // DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
